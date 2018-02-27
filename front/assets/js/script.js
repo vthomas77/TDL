ff94366d4b67ba317a0ab57cd8f8b570de78c6ca
lucyjosef <luciejoseph@ymail.com>
2018-02-27 12:37:00 +0100

    front doc step 1 : index.html package.json and main.js

---
+++ front/assets/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('[data-submit="go-to-signin"]').on('click', function(){
+        $('[data-use="signin"]').toggleClass('hidden');
+        $('[data-use="signin"]').toggleClass('flex');
+        
+        $('[data-use="login"]').toggleClass('flex');
+        $('[data-use="login"]').toggleClass('hidden');
+        //$.post('192.168.33.10:8000/login')
+    });
+    
+    $('[data-submit="back-to-login"]').on('click', function(){
+        $('[data-use="signin"]').toggleClass('hidden');
+        $('[data-use="signin"]').toggleClass('flex');
+        
+        $('[data-use="login"]').toggleClass('flex');
+        $('[data-use="login"]').toggleClass('hidden');
+        //$.post('192.168.33.10:8000/singin')
+    });
+    
+    //when submit the login
+    $('[data-submit="login"]').on('click', function(){
+        var username = $('[data-login="username"]')[0].value;
+        var password = $('[data-login="password"]')[0].value;
+        debugger;
+        $('[data-use="sidebar"] h1').append(username + " !").toUpperCase;
+    });
+    
+    //when submit sign in to create account
+    $('[data-submit="signin"]').on('click', function(){
+        var username = $('[data-signin="username"]')[0].value;
+        var password = $('[data-signin="password"]')[0].value;
+        var repeatPassword = $('[data-signin="repeat-password"]')[0].value;
+        debugger;
+        if(password === repeatPassword) {
+            console.log('OK pw');
+        } else {
+            console.log('not ok pw');
+        }
+    });
+});
