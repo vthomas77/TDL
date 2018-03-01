05c8ac596a4264e0140a3a1df67d85624c7f865a
vthomas77 <v.thomas@it-akademy.fr>
2018-03-01 17:21:51 +0100

    Create card - update front

--- front/assets/js/script.js
+++ front/assets/js/script.js
@@ -54,21 +54,59 @@ $(document).ready(function(){
         }
     });
 
+    /*
+    // Create new card
+    */
 
-    //Create new card
+    // Add/Remove asterisk for mandatory fields
+    $('[data-use="new-card-title"]').on('input', function(){
+      if ($('[data-use="new-card-title"]')[0].value != '')
+      {
+        $('[data-use="new-card-title-asterisk"]').addClass('blanked');
+      } else {
+        $('[data-use="new-card-title-asterisk"]').removeClass('blanked');
+      }
+
+    })
+
+
     $('[data-use="new-card-submit"]').on('click', function(){
       var id_user = 1;
-      var title = $('[data-use="new-card"]')[0].value;
-      var priority = $('[data-use="new-card"]')[1].value;
-      var category = $('[data-use="new-card"]')[2].value;
-      var deadline = $('[data-use="new-card"]')[3].value;
-      $.post('http://192.168.33.10:8000/createCard/' + id_user + '/' + title + '/' + priority + '/' + category + '/' + deadline,function(data){
-        if (data == true){
-          $('[data-use="notification-new-card"]').html("Card created successfully");
-        } else {
-          $('[data-use="new-card-notification"]').html(data);
+
+      // Check title field
+      var title = $('[data-use="new-card-title"]')[0].value;
+      if (title == "") {
+        $('[data-use="new-card-notification"]').html("Card title is necessary");
+      } else {
+
+        var priority = $('[data-use="new-card-priority"]')[0].value;
+
+        // Check category field
+        // Assign global by default
+        var category = $('[data-use="new-card-category"]')[0].value;
+        if (category == ""){
+          category = 'Global';
         }
-      })
+
+        // Check deadline field
+        // Assign NULL by default
+        var deadline = $('[data-use="new-card-deadline"]')[0].value;
+        if (deadline == ""){
+          deadline = 'NULL';
+        }
+
+        $.post('http://192.168.33.10:8000/createCard/' + id_user + '/' + title + '/' + priority + '/' + category + '/' + deadline,function(data){
+          if (data == true){
+            $('[data-use="new-card-notification"]').removeClass('error');
+            $('[data-use="new-card-notification"]').addClass('success');
+            $('[data-use="new-card-notification"]').html("Card created successfully");
+          } else {
+            $('[data-use="new-card-notification"]').removeClass('success');
+            $('[data-use="new-card-notification"]').addClass('error');
+            $('[data-use="new-card-notification"]').html(data);
+          }
+        })
+      }
     })
 
 
